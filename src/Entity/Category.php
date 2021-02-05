2b25b2db6d9172b8b8bf84f0528b94bf7324bb1e
Andres <afsanchez93@gmail.com>
2021-02-04 19:20:46 -0500

    Adding datatables

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -7,10 +7,12 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=CategoryRepository::class)
+ * @UniqueEntity("name")
  */
 class Category
 {
@@ -30,7 +32,6 @@ class Category
 
     /**
      * @ORM\Column(type="boolean")
-     * @Assert\NotBlank
      */
     private $active;
 
